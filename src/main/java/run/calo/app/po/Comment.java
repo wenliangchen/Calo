2e6ed38113b80e7f8dbda1d78275fbe4e273c039
wenliangchen <chen3069@purdue.edu>
2019-12-18 05:02:03 -0600

    update

--- src/main/java/run/calo/app/po/Comment.java
+++ src/main/java/run/calo/app/po/Comment.java
@@ -20,8 +20,10 @@ public class Comment {
 
     @ManyToOne
     private Blog blog;
+
     @OneToMany(mappedBy = "parentComment")
     private List<Comment> replyComments = new ArrayList<>();
+
     @ManyToOne
     private Comment parentComment;
 
